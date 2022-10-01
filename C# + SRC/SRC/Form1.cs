d19e9983f052ae64dcf935fcbc06b2450abb5f7b
XDasdazdea <50545507+Glubus@users.noreply.github.com>
2022-10-01 11:16:18 +0200

    Adding Source Code of C#

---
+++ C# + SRC/SRC/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.All(char.IsDigit) != true || (String.IsNullOrEmpty(textBox1.Text) == true))
+            {
+                MessageBox.Show("Only integer are accepted", "Error In Selecting the Length",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                List<string> test = Program.dictgenerator(checkBox1.Checked, checkBox2.Checked, checkBox3.Checked);
+                textBox2.Text = Program.generator(test, int.Parse(textBox1.Text));
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox2.Text)==false){
+                System.Windows.Forms.Clipboard.SetText(textBox2.Text);
+            }
+        
+        }
+    }
+}
---
+++ C# + SRC/SRC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            List<string> test = new List<string> { "1", "2", "3", "4" };
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        public static string generator(List<string> x, int o)
+        {
+            string otp = "";
+            int length = x.Count;
+            Random randomStep = new Random();
+            for (int i = 0; i < o; i++)
+            {
+                otp += (x[randomStep.Next(length)]).ToString();
+            }
+            return otp;
+        }
+
+        public static List<string> dictgenerator(bool z, bool y, bool o)
+        {
+            List<string> L = new List<string> {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+            if (z == true)
+            {
+                List<string> op1 = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+                L.AddRange(op1);
+            }
+            if (y == true)
+            {
+                List<string> op2 = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+                L.AddRange(op2);
+            }
+            if (o == true)
+            {
+                List<string> op3 = new List<string> { "!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "@", "^", "_", "|", "~" };
+                L.AddRange(op3);
+            }
+            return L;
+        }
+    }
+}
